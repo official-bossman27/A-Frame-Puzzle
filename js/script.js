e280646f87febc9be005aa527532191996bc0cbe
bnapolitano25 <122048979+official-bossman27@users.noreply.github.com>
2023-03-17 11:05:52 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 alert("hello world");
+
+
+AFRAME.registerComponent('update-score-text', {
+    schema: {type: 'string' 
+        
+    },
+
+    init: function () {
+      // Do something when component first attached.
+      var stringToPrint = this.data;
+      var textBox = document.querySelector("#scoretext");
+      textBox.setAttribute('value', stringToPrint);
+    },
+
+    update: function () {
+      // Do something when component's data is updated.
+    },
+
+    remove: function () {
+      // Do something the component or its entity is detached.
+    },
+
+    tick: function (time, timeDelta) {
+      // Do something on every scene tick or frame.
+    }
+});
