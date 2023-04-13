15f10ff250f277d955d2e36d3eddfc470f8d889c
bnapolitano25 <122048979+official-bossman27@users.noreply.github.com>
2023-04-13 13:04:28 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,27 +1,61 @@
-alert("hello world");
+ alert("good luck. when this window closes the mystery box will start counting down, you have 1 minute to disarm");
 
 
-AFRAME.registerComponent('update-score-text', {
-    schema: {type: 'string' 
-        
-    },
+ setTimeout(() => {
+  alert('You failed to disarm the bomb in time!');
+  // Replace this line with code to switch to the death screen
+}, 60000);
 
-    init: function () {
-      // Do something when component first attached.
-      var stringToPrint = this.data;
-      var textBox = document.querySelector("#scoretext");
-      textBox.setAttribute('value', stringToPrint);
-    },
 
-    update: function () {
-      // Do something when component's data is updated.
-    },
+    function movenose() {
+ 
+      var button = document.querySelector('#button');
+      var nose = document.querySelector('#nose');
 
-    remove: function () {
-      // Do something the component or its entity is detached.
-    },
-
-    tick: function (time, timeDelta) {
-      // Do something on every scene tick or frame.
+      nose.setAttribute('animation', 'property: position; to: -5 1 -6; dur: 1000; easing: linear');
+      button.setAttribute('animation', 'property: position; to: -3 1.4 -3; dur: 100; easing: linear');
     }
-});
+
+    function move2() {
+ 
+      var button = document.querySelector('#button2');
+      var box = document.querySelector('#keybox');
+
+      box.setAttribute('animation', 'property: position; to: 0 1 -4; dur: 1000; easing: linear');
+      button.setAttribute('animation', 'property: position; to: -2.65 1 -6; dur: 100; easing: linear');
+      
+    }
+
+
+    function final() {
+
+      var key = document.querySelector('#keybox');
+
+      var button3 = document.querySelector('#button3');
+  
+  
+      var keyFound = false;
+  
+      key.addEventListener('click' , () => {
+      
+        keyFound = true;
+  
+        alert('you found the key! press the button quick');
+  
+      });
+
+      button3.addEventListener('click', () => {
+        if (keyFound) {
+
+        button3.setAttribute('animation', 'property: position; to: 0 1.05 0.4; dur: 100; easing: linear');
+        alert('you disarmed the bonb!')
+
+        
+
+     
+      }
+
+      });
+
+    }
+
